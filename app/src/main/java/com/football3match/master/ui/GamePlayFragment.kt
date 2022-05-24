8fb4b228153a7fdd3dff08fbf9657f738be61df2
bereket-kassahun <bereket_kasahun@yahoo.com>
2022-05-24 10:41:48 +0300

    feat: finishing up game play fragment

--- app/src/main/java/com/football3match/master/ui/GamePlayFragment.kt
+++ app/src/main/java/com/football3match/master/ui/GamePlayFragment.kt
@@ -1,6 +1,7 @@
 package com.football3match.master.ui
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +17,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.football3match.master.R
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-
+import java.nio.InvalidMarkException
 
 
 class GamePlayFragment : Fragment() {
@@ -34,19 +36,25 @@ class GamePlayFragment : Fragment() {
             R.drawable.group_2,
             R.drawable.cards,
             R.drawable.trophy,
-            R.drawable.foot_ball,
-            R.drawable.stop_watch
+            R.drawable.tickets
         )
+
+
+        val ball = R.drawable.foot_ball
+        val stopWatch = R.drawable.stop_watch
+        val leftGlove = R.drawable.left_glove
+        val rightGlove = R.drawable.right_glove
     }
 
     private lateinit var viewModel: GamePlayViewModel
+    private lateinit var goalSound: MediaPlayer
+    private lateinit var disappearSound: MediaPlayer
+    private lateinit var timerCollectedSound: MediaPlayer
+    private lateinit var timeIsUpSound: MediaPlayer
 
-    val ball = R.drawable.foot_ball
-    val stopWatch = R.drawable.stop_watch
-    val leftGlove = R.drawable.left_glove
-    val rightGlove = R.drawable.right_glove
 
     lateinit var boardView: GridLayout
+    lateinit var goalView: GridLayout
     val board = Array<Int>(ROWS* COLUMNS){
         0
     }
@@ -55,6 +63,8 @@ class GamePlayFragment : Fragment() {
     lateinit var itemParams: RelativeLayout.LayoutParams
     lateinit var startPause: ImageButton
     lateinit var sound: ImageButton
+
+    var firstTime = true
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -63,6 +73,7 @@ class GamePlayFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(GamePlayViewModel::class.java)
         val root = inflater.inflate(R.layout.game_play_fragment, container, false)
         boardView = root.findViewById(R.id.board)
+        goalView = root.findViewById(R.id.goal)
         startPause = root.findViewById(R.id.start_pause)
         sound = root.findViewById(R.id.sound)
 
@@ -72,12 +83,21 @@ class GamePlayFragment : Fragment() {
         sound.setOnClickListener {
         }
 
-
+        lifecycleScope.launch {
+            viewModel.playMusic.collectLatest {
+                playSound(it)
+            }
+        }
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        goalSound = MediaPlayer.create(requireContext(), R.raw.goal)
+        disappearSound = MediaPlayer.create(requireContext(), R.raw.element_disappeared)
+        timerCollectedSound = MediaPlayer.create(requireContext(), R.raw.got_timer)
+        timeIsUpSound = MediaPlayer.create(requireContext(), R.raw.time_is_up)
 
 
         boardView.post {
@@ -87,20 +107,39 @@ class GamePlayFragment : Fragment() {
             lifecycleScope.launch {
                 viewModel.checkBoard.collect {
 
-                    viewModel.checkRow(boardView)
-                    viewModel.checkColumn(boardView)
+                    val rCheck = viewModel.checkRow(boardView)
+                    val cCheck = viewModel.checkColumn(boardView)
                     val gaps = viewModel.brigDownPieces(boardView)
                     viewModel.fillGaps(gaps, boardView)
+                    if(!rCheck && !cCheck && firstTime){
+                        //adding the ball
+                        val x = (0..COLUMNS).random()
+                        (boardView[x] as ImageView).setImageResource(ball)
+                        (boardView[x] as ImageView).tag = ball
+                        firstTime = false
+                    }
                 }
             }
         }
+        goalView.post{
+            itemParams = RelativeLayout.LayoutParams(boardView.width/ COLUMNS,boardView.height/ ROWS)
+            for(i in 0 until COLUMNS){
+                val imageView = ImageView(requireContext())
+                imageView.setImageResource(0)
+                imageView.tag = 0
+                imageView.layoutParams = itemParams
+                goalView.addView(imageView)
+            }
+            (goalView[2] as ImageView).setImageResource(leftGlove)
+            (goalView[2] as ImageView).tag = leftGlove
+            (goalView[3] as ImageView).setImageResource(rightGlove)
+            (goalView[3] as ImageView).tag = rightGlove
 
-
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
     fun populate(){
-
         for(i in 0 until ROWS* COLUMNS){
             val rand = (items.indices).random()
             val imageView = ImageView(requireContext())
@@ -118,6 +157,7 @@ class GamePlayFragment : Fragment() {
                     img1Animation.duration = 300
                     img2Animation.duration = 300
                     viewModel.swap(imageView, img2, img1Animation, img2Animation)
+                    viewModel.moveGloves(goalView, boardView)
                 }
 
                 override fun swapBottom() {
@@ -131,20 +171,22 @@ class GamePlayFragment : Fragment() {
                     img1Animation.duration = 300
                     img2Animation.duration = 300
                     viewModel.swap(imageView, img2, img1Animation, img2Animation)
+                    viewModel.moveGloves(goalView, boardView)
                 }
 
                 override fun swapLeft() {
                     super.swapLeft()
-                    val leftEdges = listOf<Int>(0,6,12,18,24,32,38,44)
+                    val leftEdges = listOf<Int>(0,6,12,18,24,30,36,42)
                     if(leftEdges.contains(i))
                         return
                     val img2 = boardView[i - 1] as ImageView
 
                     val img1Animation = TranslateAnimation( -1f*imageView.width, 0f,0f, 0f)
-                    val img2Animation = TranslateAnimation(imageView.width.toFloat(), 0f, 0f, 0f, )
+                    val img2Animation = TranslateAnimation(imageView.width.toFloat(), 0f, 0f, 0f)
                     img1Animation.duration = 300
                     img2Animation.duration = 300
                     viewModel.swap(imageView, img2, img1Animation, img2Animation)
+                    viewModel.moveGloves(goalView, boardView)
                 }
 
                 override fun swapRight() {
@@ -158,9 +200,80 @@ class GamePlayFragment : Fragment() {
                     img1Animation.duration = 300
                     img2Animation.duration = 300
                     viewModel.swap(imageView, img2, img1Animation, img2Animation)
+                    viewModel.moveGloves(goalView, boardView)
                 }
             })
             boardView.addView(imageView)
         }
+
+        //adding stop watch
+        if(!viewModel.checkTimerExists(boardView)){
+            val tmp = (0..9).random()
+            if(tmp % 2 == 0){
+                val y = (0 until ROWS).random()
+                val x = (0 until COLUMNS).random()
+                (boardView[x+(y*COLUMNS)] as ImageView).setImageResource(stopWatch)
+                (boardView[x+(y*COLUMNS)] as ImageView).tag = stopWatch
+            }
+        }
+
+
+
     }
+
+
+    private fun playSound(type: Int){
+        when(type){
+            1 -> {
+                try {
+                    if (goalSound.isPlaying) {
+                        goalSound.stop()
+                        goalSound.release()
+                        goalSound = MediaPlayer.create(context, R.raw.goal)
+                    }
+                    goalSound.start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            2 -> {
+                try {
+                    if (disappearSound.isPlaying) {
+                        disappearSound.stop()
+                        disappearSound.release()
+                        disappearSound = MediaPlayer.create(context, R.raw.element_disappeared)
+                    }
+                    disappearSound.start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            3 -> {
+                try {
+                    if (timerCollectedSound.isPlaying) {
+                        timerCollectedSound.stop()
+                        timerCollectedSound.release()
+                        timerCollectedSound = MediaPlayer.create(context, R.raw.got_timer)
+                    }
+                    timerCollectedSound.start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            4 -> {
+                try {
+                    if (timeIsUpSound.isPlaying) {
+                        timeIsUpSound.stop()
+                        timeIsUpSound.release()
+                        timeIsUpSound = MediaPlayer.create(context, R.raw.time_is_up)
+                    }
+                    timeIsUpSound.start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/football3match/master/ui/GamePlayViewModel.kt
+++ app/src/main/java/com/football3match/master/ui/GamePlayViewModel.kt
@@ -1,129 +1,217 @@
 package com.football3match.master.ui
 
-import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
 import android.widget.GridLayout
 import android.widget.ImageView
 import androidx.core.view.get
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.football3match.master.ui.GamePlayFragment.Companion.COLUMNS
 import com.football3match.master.ui.GamePlayFragment.Companion.ROWS
+import com.football3match.master.ui.GamePlayFragment.Companion.ball
 import com.football3match.master.ui.GamePlayFragment.Companion.items
+import com.football3match.master.ui.GamePlayFragment.Companion.leftGlove
+import com.football3match.master.ui.GamePlayFragment.Companion.rightGlove
+import com.football3match.master.ui.GamePlayFragment.Companion.stopWatch
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class GamePlayViewModel : ViewModel() {
 
 
-    init {
+    private val _playMusic = MutableSharedFlow<Int>()
+    val playMusic = _playMusic.asSharedFlow()
 
+    init {
+    }
+
+    fun emitMusicType(type: Int) {
+        viewModelScope.launch {
+            _playMusic.emit(type)
+        }
     }
 
     val checkBoard = flow<Int> {
-        while(true){
+        while (true) {
             emit(0)
             delay(500)
         }
     }
+
     // checks row and L shaped ones
-    fun checkRow(gridLayout: GridLayout){
+    fun checkRow(gridLayout: GridLayout): Boolean {
         var count = 0
-        var verticalCount = 0
+        var verticalCountAbove = 0
+        var verticalCountBelow = 0
         var currentDrawable = 0
         var currentRowItems = 0
-        for(y in 0 until ROWS){
-            currentRowItems = y*COLUMNS
-            for(x in 0..COLUMNS -2){
-                currentDrawable = gridLayout[currentRowItems+x].tag as Int
-                for(z in x+1 until COLUMNS){
-                    if(currentDrawable == gridLayout[currentRowItems+z].tag as Int){
+        var ret = false
+        for (y in 0 until ROWS) {
+            currentRowItems = y * COLUMNS
+            for (x in 0..COLUMNS - 2) {
+                currentDrawable = gridLayout[currentRowItems + x].tag as Int
+                for (z in x + 1 until COLUMNS) {
+                    if (currentDrawable == gridLayout[currentRowItems + z].tag as Int
+                    ) {
                         count++
-                    }else{
+                    } else {
                         break
                     }
                 }
-                if(count >= 2){
-                    for(i in x..x+count){
-                        (gridLayout[currentRowItems+i] as ImageView).setImageResource(0)
-                        gridLayout[currentRowItems+i].tag = 0
-                    }
+                if (count >= 2) {
+                    for (i in x..x + count) {
+                        (gridLayout[currentRowItems + i] as ImageView).setImageResource(0)
+                        gridLayout[currentRowItems + i].tag = 0
 
-                    for(w in y+1 until ROWS){
-                        if(currentDrawable == gridLayout[x+(w* COLUMNS)].tag as Int){
-                            verticalCount++
-                        }else{
-                            break
+                        val ret = checkIfAdjecentTimerExists(gridLayout, i, y)
+                        if(ret.first){
+                            gridLayout[(ret.second.second * COLUMNS) + ret.second.first].tag = 0
+                            (gridLayout[(ret.second.second * COLUMNS) + ret.second.first] as ImageView).setImageResource(0)
+                            emitMusicType(3)
                         }
-                    }
-                    if(verticalCount >= 2){
-                        for(i in y+1..y+verticalCount){
-                            (gridLayout[x+(i*COLUMNS)] as ImageView).setImageResource(0)
-                            gridLayout[x+(i*COLUMNS)].tag = 0
+                        //start of untested code
+                        for (w in y + 1 until ROWS) {
+                            if (currentDrawable == gridLayout[i + (w * COLUMNS)].tag as Int
+                            ) {
+                                verticalCountBelow++
+                            } else {
+                                break
+                            }
                         }
+                        for (w in y - 1 downTo 0) {
+                            if (currentDrawable == gridLayout[i + (w * COLUMNS)].tag as Int
+                            ) {
+                                verticalCountAbove++
+                            } else {
+                                break
+                            }
+                        }
+
+                        if (verticalCountAbove + verticalCountBelow >= 2) {
+                            for (j in y - verticalCountAbove..y + verticalCountBelow) {
+                                (gridLayout[i + (j * COLUMNS)] as ImageView).setImageResource(0)
+                                gridLayout[i + (j * COLUMNS)].tag = 0
+
+                                val ret = checkIfAdjecentTimerExists(gridLayout, i, j)
+                                if(ret.first){
+                                    gridLayout[(ret.second.second * COLUMNS) + ret.second.first].tag = 0
+                                    (gridLayout[(ret.second.second * COLUMNS) + ret.second.first] as ImageView).setImageResource(0)
+                                    emitMusicType(3)
+                                }
+                            }
+                        }
+
+                        verticalCountAbove = 0
+                        verticalCountBelow = 0
+
+
+                        //                        Log.e("VERTICAL", "$verticalCountAbove ${y-1} $x")
+//                        if(verticalCountAbove >= 2){
+//                            for(j in y-1 downTo y-verticalCountAbove){
+//                                (gridLayout[i+(j*COLUMNS)] as ImageView).setImageResource(0)
+//                                gridLayout[i+(j*COLUMNS)].tag = 0
+//                            }
+//                        }
+
+//                        if(verticalCountBelow >= 2){
+//                            for(j in y+1..y+verticalCountBelow){
+//                                (gridLayout[i+(j*COLUMNS)] as ImageView).setImageResource(0)
+//                                gridLayout[i+(j*COLUMNS)].tag = 0
+//                            }
+//                        }
+
+
+                        //end of untested code
                     }
+                    ret = true
+                    emitMusicType(2)
                 }
                 count = 0
             }
         }
+
+        return ret
     }
 
     //checks column
-    fun checkColumn(gridLayout: GridLayout){
+    fun checkColumn(gridLayout: GridLayout): Boolean {
         var currentDrawable = 0
         var count = 0
-        for(x in 0 until COLUMNS){
-            for(y in 0 until ROWS){
-                currentDrawable = gridLayout[(y*COLUMNS)+x].tag as Int
-                for(z in y+1 until ROWS){
-                    if(currentDrawable == gridLayout[(z*COLUMNS)+x].tag as Int)
+        var ret = false
+        for (x in 0 until COLUMNS) {
+            for (y in 0 until ROWS) {
+                currentDrawable = gridLayout[(y * COLUMNS) + x].tag as Int
+                for (z in y + 1 until ROWS) {
+                    if (currentDrawable == gridLayout[(z * COLUMNS) + x].tag as Int)
                         count++
                     else
                         break
                 }
-                if(count >= 2){
-                    for(i in y..y+count){
-                        gridLayout[(i*COLUMNS)+x].tag = 0
-                        (gridLayout[(i*COLUMNS)+x] as ImageView).setImageResource(0)
+                if (count >= 2) {
+                    for (i in y..y + count) {
+                        gridLayout[(i * COLUMNS) + x].tag = 0
+                        (gridLayout[(i * COLUMNS) + x] as ImageView).setImageResource(0)
+
+                        val ret = checkIfAdjecentTimerExists(gridLayout, x, i)
+                        if(ret.first){
+                            gridLayout[(ret.second.second * COLUMNS) + ret.second.first].tag = 0
+                            (gridLayout[(ret.second.second * COLUMNS) + ret.second.first] as ImageView).setImageResource(0)
+                            emitMusicType(3)
+                        }
                     }
+                    emitMusicType(2)
+                    ret = true
                 }
                 count = 0
             }
         }
+        return ret
     }
 
     //fills the gap from bottom to up and returns each column gaps
-    fun brigDownPieces(gridLayout: GridLayout): Array<Int>{
+    fun brigDownPieces(gridLayout: GridLayout): Array<Int> {
         var currentDrawable = 0
         var count = 0
         var gapFound = false
         val itemHeight = (gridLayout[0] as ImageView).height
-        val gaps = Array<Int>(COLUMNS){
+        val gaps = Array<Int>(COLUMNS) {
             0
         }
-        for(x in 0 until COLUMNS){
-            for(y in ROWS-1 downTo  0){
-                currentDrawable = gridLayout[(y* COLUMNS)+x].tag as Int
-                if(currentDrawable == 0 && !gapFound){
+        for (x in 0 until COLUMNS) {
+            for (y in ROWS - 1 downTo 0) {
+                currentDrawable = gridLayout[(y * COLUMNS) + x].tag as Int
+                if (currentDrawable == 0 && !gapFound) {
                     gapFound = true
-                    for(z in y downTo 0){
-                        if(gridLayout[(z* COLUMNS)+x].tag == 0)
+                    for (z in y downTo 0) {
+                        if (gridLayout[(z * COLUMNS) + x].tag == 0)
                             count++
                         else
                             break
                     }
                 }
-                if(count > 0){
-                    val animation = TranslateAnimation(0f,0f,-1f*(count*itemHeight),0f)
-                    animation.duration = 400
-                    for(i in 0..y-count){
-                        if(y-i-count >= 0){
-                            gridLayout[((y-i)* COLUMNS)+x].tag = gridLayout[((y-i-count)* COLUMNS)+x].tag
-                            (gridLayout[((y-i)* COLUMNS)+x] as ImageView).setImageResource(gridLayout[((y-i-count)* COLUMNS)+x].tag as Int)
-                            (gridLayout[((y-i)* COLUMNS)+x] as ImageView).startAnimation(animation)
-                            gridLayout[((y-i-count)* COLUMNS)+x].tag = 0
-                            (gridLayout[((y-i-count)* COLUMNS)+x] as ImageView).setImageResource(0)
+                if (count > 0) {
+                    val animation = TranslateAnimation(0f, 0f, -1f * (count * itemHeight), 0f)
+                    animation.duration = if (count == 1) 400 else 400 + 50L * count
+                    for (i in 0..y - count) {
+                        if (y - i - count >= 0) {
+                            gridLayout[((y - i) * COLUMNS) + x].tag =
+                                gridLayout[((y - i - count) * COLUMNS) + x].tag
+                            (gridLayout[((y - i) * COLUMNS) + x] as ImageView).setImageResource(
+                                gridLayout[((y - i - count) * COLUMNS) + x].tag as Int
+                            )
+                            (gridLayout[((y - i) * COLUMNS) + x] as ImageView).startAnimation(
+                                animation
+                            )
+                            gridLayout[((y - i - count) * COLUMNS) + x].tag = 0
+                            (gridLayout[((y - i - count) * COLUMNS) + x] as ImageView).setImageResource(
+                                0
+                            )
                         }
                     }
                     gaps[x] = count
@@ -135,21 +223,28 @@ class GamePlayViewModel : ViewModel() {
         return gaps
     }
 
-    fun fillGaps(gaps: Array<Int>, gridLayout: GridLayout){
+    fun fillGaps(gaps: Array<Int>, gridLayout: GridLayout) {
         val itemHeight = (gridLayout[0] as ImageView).height
-        for(x in 0 until COLUMNS){
-            for(y in 0 until gaps[x]){
-                val animation = TranslateAnimation(0f,0f,-1f*(y*itemHeight),0f)
-                animation.duration = 400
+        for (x in 0 until COLUMNS) {
+            for (y in 0 until gaps[x]) {
+                val animation = TranslateAnimation(0f, 0f, -1f * (y * itemHeight), 0f)
+                animation.duration = if (gaps[x] == 1) 400 else 400 + 50L * gaps[x]
                 val rand = (items.indices).random()
-                (gridLayout[y* COLUMNS+x] as ImageView).setImageResource(items[rand])
-                (gridLayout[y* COLUMNS+x] as ImageView).tag = items[rand]
-                (gridLayout[y* COLUMNS+x] as ImageView).startAnimation(animation)
+                (gridLayout[y * COLUMNS + x] as ImageView).setImageResource(items[rand])
+                (gridLayout[y * COLUMNS + x] as ImageView).tag = items[rand]
+                (gridLayout[y * COLUMNS + x] as ImageView).startAnimation(animation)
+                //adding stopwatch if it doesn't exist
+                if ((0..6).random() == 0 && !checkTimerExists(gridLayout)) {
+                    (gridLayout[y * COLUMNS + x] as ImageView).setImageResource(stopWatch)
+                    (gridLayout[y * COLUMNS + x] as ImageView).tag = stopWatch
+                    (gridLayout[y * COLUMNS + x] as ImageView).startAnimation(animation)
+                }
             }
         }
+
     }
 
-    fun swap(img1: ImageView, img2: ImageView, img1Animation: Animation, img2Animation: Animation){
+    fun swap(img1: ImageView, img2: ImageView, img1Animation: Animation, img2Animation: Animation) {
         val tmp: Int = img2.tag as Int
         img2.setImageResource(img1.tag as Int)
         img2.tag = img1.tag
@@ -157,6 +252,84 @@ class GamePlayViewModel : ViewModel() {
         img1.tag = tmp
         img1.startAnimation(img1Animation)
         img2.startAnimation(img2Animation)
+    }
 
+    fun checkTimerExists(gridLayout: GridLayout): Boolean {
+        for (x in 0 until COLUMNS) {
+            for (y in 0 until ROWS) {
+                if ((gridLayout[x + (y * COLUMNS)] as ImageView).tag == GamePlayFragment.stopWatch) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun checkIfAdjecentTimerExists(gridLayout: GridLayout, x: Int, y: Int): Pair<Boolean, Pair<Int,Int>>{
+        if(x-1 >= 0 && gridLayout[(x-1)+(y* COLUMNS)].tag as Int == stopWatch)
+            return Pair(true, Pair(x-1, y))
+        if(x+1 < COLUMNS && gridLayout[(x+1)+(y* COLUMNS)].tag as Int == stopWatch)
+            return Pair(true, Pair(x+1, y))
+        if(y-1 >= 0 && gridLayout[x+((y-1)* COLUMNS)].tag as Int == stopWatch)
+            return Pair(true, Pair(x, y-1))
+        if(y+1 < ROWS && gridLayout[x+((y+1)* COLUMNS)].tag as Int == stopWatch)
+            return Pair(true, Pair(x, y+1))
+        return Pair(false, Pair(x,y))
+    }
+
+
+    fun moveGloves(gridLayout: GridLayout, goal: GridLayout){
+        val canMoveLeft = (gridLayout[0] as ImageView).tag == 0
+        val canMoveRight = (gridLayout[5] as ImageView).tag == 0
+        var currentPos = 0
+        var tmp = 0
+        for(i in 0 until COLUMNS){
+            if((gridLayout[i] as ImageView).tag == leftGlove && (gridLayout[i+1] as ImageView).tag == rightGlove){
+                currentPos = i
+                tmp = i
+                (gridLayout[i] as ImageView).tag = 0
+                (gridLayout[i] as ImageView).setImageResource(0)
+                (gridLayout[i+1] as ImageView).tag = 0
+                (gridLayout[i+1] as ImageView).setImageResource(0)
+                break
+            }
+        }
+        if(canMoveLeft && canMoveRight){
+            when((0..2).random()){
+                0 -> currentPos--
+                2 -> currentPos++
+            }
+        }else if(canMoveLeft){
+            currentPos--
+        }else if(canMoveRight){
+            currentPos++
+        }
+
+        (gridLayout[currentPos] as ImageView).setImageResource(leftGlove)
+        (gridLayout[currentPos] as ImageView).tag = leftGlove
+        (gridLayout[currentPos+1] as ImageView).setImageResource(rightGlove)
+        (gridLayout[currentPos+1] as ImageView).tag = rightGlove
+
+        val animation = if(tmp > currentPos){
+            TranslateAnimation(goal.width/ COLUMNS.toFloat(), 0f, 0f,0f)
+        }else{
+            TranslateAnimation( 0f,goal.width/ COLUMNS.toFloat(), 0f,0f)
+        }
+        animation.duration = 400
+        (gridLayout[currentPos] as ImageView).startAnimation(animation)
+
+        if(lastLineBallLocation(goal) == currentPos){
+            emitMusicType(1)
+        }
+
+    }
+
+    fun lastLineBallLocation(gridLayout: GridLayout): Int{
+        val y = ROWS-1
+        for(x in 0 until COLUMNS){
+            if(gridLayout[y* COLUMNS + x].tag == ball)
+                return x
+        }
+        return -1
     }
 }
